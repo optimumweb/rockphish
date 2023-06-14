96efdbf14326505f5e570c26cd2cbf342a2e0dc5
Jonathan Roy <jroy66@gmail.com>
2023-06-14 12:27:43 -0400

    add options to force or resend

--- app/Console/Commands/SendCampaign.php
+++ app/Console/Commands/SendCampaign.php
@@ -7,51 +7,65 @@ use Illuminate\Console\Command;
 
 class SendCampaign extends Command
 {
-    protected $signature = 'app:campaign:send {campaign}';
+    protected $signature = 'app:campaign:send {campaign} {--force} {--resend}';
 
     protected $description = 'Sends campaign emails to targets.';
 
     public function handle()
     {
         $campaignUuid = $this->argument('campaign');
+
+        if (! isset($campaignUuid)) {
+            $this->error("You must supply a campaign UUID");
+            return Command::INVALID;
+        }
+
         $campaign = Campaign::where('uuid', $campaignUuid)->first();
 
         if (! isset($campaign)) {
             $this->error("No campaign with UUID '{$campaignUuid}'");
-            return Command::FAILURE;
+            return Command::INVALID;
         }
 
         $targetsCount = $campaign->targets->count();
 
-        if ($targetsCount > 0) {
-            if ($this->confirm("Proceed to send campaign to {$targetsCount} targets?")) {
-                $sent = 0;
-                $errors = 0;
-
-                foreach ($campaign->targets as $target) {
-                    try {
-                        $email = $campaign->emails()->make([
-                            'target' => $target,
-                        ]);
-                        $email->saveOrFail();
-                        $email->send();
-
-                        $sent++;
-
-                        $this->info("Email '{$email}' sent to '{$target}'");
-                    } catch (\Exception $e) {
-                        $errors++;
-                        $this->error("Email '{$email}' could not be sent to '{$target}': {$e}");
-                    }
-                }
-
-                $this->line("Result: {$sent} sent / {$errors} errors");
-
-                return $errors > 0 ? Command::FAILURE : Command::SUCCESS;
-            }
-        } else {
+        if ($targetsCount === 0) {
             $this->error("No targets for Campaign '{$campaign}'");
-            return Command::FAILURE;
+            return Command::INVALID;
         }
+
+        if ($this->option('force') || $this->confirm("Proceed to send campaign to {$targetsCount} targets?")) {
+            $sent = 0;
+            $skipped = 0;
+            $errors = 0;
+
+            foreach ($campaign->targets as $target) {
+                try {
+                    $email = $campaign->emails()->firstOrCreate([
+                        'target' => $target,
+                    ]);
+
+                    if ($this->option('resend') || ! isset($email->sent_at)) {
+                        $email->send();
+                        $sent++;
+                        $this->info("Email '{$email}' sent to '{$target}'");
+                    } else {
+                        $skipped++;
+                        $this->line("Email '{$email}' already sent to '{$target}'");
+                    }
+                } catch (\Exception $e) {
+                    $errors++;
+                    $this->error("Email '{$email}' could not be sent to '{$target}': {$e}");
+                }
+            }
+
+            $this->line("Result: {$sent} sent / {$skipped} skipped / {$errors} errors", $errors > 0 ? 'error' : 'info');
+
+            if ($errors > 0) {
+                return Command::FAILURE;
+            }
+        }
+
+        return Command::SUCCESS;
     }
 }
